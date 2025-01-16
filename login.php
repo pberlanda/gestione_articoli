3aaa34b484f6f9e6c2ade28e94a34e6e880fe6ba
paolo berlanda <paolo.berlanda@marconirovereto.it>
2025-01-16 07:18:03 +0100

    Aggiunta CSS, in nuovo.php controllo esistenza codice usato e modifica gestione errori

--- login.php
+++ login.php
@@ -57,11 +57,12 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>gestione articoli login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Gestione articoli - Login</h1>
 
-    <form method="POST">
+    <form class="container" method="POST">
         <input type="text" name="username" placeholder="Inserisci username" required>
         <input type="password" name="password" placeholder="Inserisci password"required>
 
